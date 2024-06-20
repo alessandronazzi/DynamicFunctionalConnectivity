e92f9dd2e211523839629a8a214be7843f96c6f5
alessandronazzi <122542576+alessandronazzi@users.noreply.github.com>
2024-06-20 15:20:39 +0200

    Update boxp_dyn.m

--- Functions/boxp_dyn.m
+++ Functions/boxp_dyn.m
@@ -1,4 +1,4 @@
-function [ft,dt] = boxp_dyn(n_sbj,idx,l,width_w)
+function [ft,dt] = boxp_dyn(n_sbj,idx,l,width_w,K)
 
     n_window = 2*(l/2 - width_w + 1);
     ss = zeros(1, n_sbj) + n_window;
@@ -8,12 +8,31 @@ function [ft,dt] = boxp_dyn(n_sbj,idx,l,width_w)
 
     for i=1:length(idx_sbj)
         sbj = idx_sbj{i};
-        [Fraction_times,Dwell_times,~] = FC_dyn(sbj,5);
+        [Fraction_times,Dwell_times,~] = FC_dyn(sbj,K);
         ft = [ft, Fraction_times];
         dt = [dt, Dwell_times];
     end
 
     ft = ft';
+    dt = dt*2.1;
     dt = dt';
     
+    ft([236 487],:)=[];
+    dt([236 487],:)=[];
+    
+    figure()
+    violin(ft,'facecolor',[0.5 0.7 1],'medc',[])
+    title('Fraction times')
+    xlabel('DFS')
+    xticks([1 2])
+    ylabel('%')
+    set(gca,'FontSize',15)
+    
+    figure()
+    violin(dt,'facecolor',[0.5 0.7 1],'medc',[])
+    title('Dwell times')
+    xlabel('DFS')
+    xticks([1 2])
+    ylabel('seconds')
+    set(gca,'FontSize',15)
 end    
