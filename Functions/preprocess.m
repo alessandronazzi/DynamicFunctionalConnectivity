a6538dfd83175b8b257d3b5f38841229034e1923
alessandronazzi <122542576+alessandronazzi@users.noreply.github.com>
2024-06-20 15:33:17 +0200

    Update preprocess.m

--- Functions/preprocess.m
+++ Functions/preprocess.m
@@ -3,21 +3,18 @@ function [timeseries_all] = preprocess(varargin)
 
     nargin=length(varargin);
     timeseries_all=varargin{1};
-
     atlas=varargin{2};
 
     if(strcmp(atlas,'GordonLaumann') || strcmp(atlas,'TianS1') || strcmp(atlas,'TianS4') || strcmp(atlas,'Buckner') || strcmp(atlas,'Morel') )
         load('/home/allegra/hcp_padova/results/GordonLaumann/NET_info_reduction2.mat');
     end
     
-
     glob=true;
     for i=1:nargin
         if ischar(varargin{i}) && strcmp(varargin{i},'noglob')
 	   glob=false;
 	end
     end
-
 
     % DETREND % 
 
@@ -40,7 +37,6 @@ function [timeseries_all] = preprocess(varargin)
 
     timeseries_all = timeseries_det;
 
-
     % BAND-PASS FILTER % 
     
     bpss_low=0.009;
@@ -49,18 +45,14 @@ function [timeseries_all] = preprocess(varargin)
     filterorder=1;
     lopass=bpss_low/(0.5/TR);
     hipass=bpss_high/(0.5/TR);
-    
     timeseries_lpf = zeros(w_timeseries,l_timeseries,n_sbj);
     
     for i=1:size(timeseries_all,3) 
         sbj_n = timeseries_all(:,:,i);
-
         [butta, buttb] = butter(filterorder,[lopass hipass]);
         sbj_n = [filtfilt(butta,buttb,sbj_n(:,1:l_timeseries/2)')',filtfilt(butta,buttb,sbj_n(:,l_timeseries/2+1:l_timeseries)')'];
         timeseries_lpf(:,:,i) = sbj_n;
-
     end
-
 
     timeseries_all = timeseries_lpf;
 
@@ -68,13 +60,11 @@ function [timeseries_all] = preprocess(varargin)
 
     if(glob)
         timeseries_noG = zeros(w_timeseries,l_timeseries,n_sbj);
-
         for i=1:size(timeseries_all,3)
             sbj = timeseries_all(:,:,i);
             global_signal = mean(sbj,1);
             beta_g = sbj*pinv(global_signal);
             res_sbj = sbj - beta_g*global_signal;
-
             timeseries_noG(:,:,i) = res_sbj;
         end
         timeseries_all = timeseries_noG;
@@ -86,7 +76,6 @@ function [timeseries_all] = preprocess(varargin)
 
         atlas_90 = NET_new.final_index;
         conv_areas = [];
-	
 	timeseries_red = zeros(size(timeseries_all,1)-253,size(timeseries_all,2),size(timeseries_all,3));
 
         for i=1:size(timeseries_all,3)
@@ -112,9 +101,7 @@ function [timeseries_all] = preprocess(varargin)
     
     elseif strcmp(atlas,'TianS1') || strcmp(atlas,'TianS4') || strcmp(atlas,'Buckner') || strcmp(atlas,'Morel') 
         atlas_90 = NET_new.final_index(1:71);
-   
         conv_areas = [];
-	
 	timeseries_red = zeros(size(timeseries_all,1)-253,size(timeseries_all,2),size(timeseries_all,3));
 
         for i=1:size(timeseries_all,3)
