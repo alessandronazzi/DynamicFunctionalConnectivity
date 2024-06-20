eabf156097de453945332e65259e403a25869c85
alessandronazzi <122542576+alessandronazzi@users.noreply.github.com>
2024-06-20 15:42:57 +0200

    Update optimalclust.m

--- Functions/optimalclust.m
+++ Functions/optimalclust.m
@@ -1,22 +1,9 @@
-function [optim_k,idx_opt,centroids_opt,mean_sil,std_sil] = optimalclust(upt_vmat,Npoints,Nrand,K)
 
-   disp('Clustering');
-
-    idx = {};
-
-    for i=2:K
-        disp(sprintf('k=%d',i));
-        idx{i} = [];
-        [idxs,centroids,upts] = K_cluster(upt_vmat,i);
-        idx{i} = idxs;
-        centroid{i} = centroids;
-    end
+function [optim_k,idx_opt,centroids_opt,mean_sil,std_sil] = optimalclust(upts,idx,centroid,Npoints,Nrand,K)
 
     sil = {};
-
     disp('Compute silhouette');
-
-    %tic
+    
     for i=2:K
         disp(sprintf('k=%d',i));
         silh_vals=zeros(Nrand,1);
@@ -24,21 +11,18 @@ function [optim_k,idx_opt,centroids_opt,mean_sil,std_sil] = optimalclust(upt_vma
         parfor k=1:Nrand
             points=randperm(size(upts,1));
             points=points(1:Npoints);
-            silh_vals(k)=mean(silhouette(upts(points,:),indices(points)));
+            silh_vals(k)=mean(silhouette(upts(:,points)',indices(points)));
         end
-            sil{i}=silh_vals;
+	    sil{i}=silh_vals;
     end
-    %toc
-
+    
     for i=1:K
         mean_sil(i)=mean(sil{i},'omitnan');
         std_sil(i)=std(sil{i},'omitnan');
     end
-
+    
     [~,optim_k] = max(mean_sil);
     idx_opt = idx{optim_k};
     centroids_opt = centroid{optim_k};
 
-    %errorbar(mean_sil,std_sil);
-
-end
+end                             
