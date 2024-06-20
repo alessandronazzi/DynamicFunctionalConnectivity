2494a89eb2af057e63e3ce38f07637bcad26aea2
alessandronazzi <122542576+alessandronazzi@users.noreply.github.com>
2024-06-20 15:02:08 +0200

    Update K_cluster.m

--- Functions/K_cluster.m
+++ Functions/K_cluster.m
@@ -1,11 +1,30 @@
 
-function [idx,centroids,upts] = K_cluster(upt_vmat,K,distance)
+function [idx,idx_clean,centroids,upts] = K_cluster(upt_vmat,K,distance)
     upts = transpose(upt_vmat);
     
     if strcmp(distance,'Euclidean')
         [idx, centroids] = kmeans(upts,K,'MaxIter',1000);
     else
         [idx, centroids] = kmeans(upts,K,'Distance','correlation','MaxIter',1000);
+    end
+
+    idx_clean = idx;
+
+
+    if strcmp(distance,'Euclidean')
+        Dth=min(pdist(centroids));
+    else
+        Dth=min(pdist(centroids,'Correlation'));
+    end
+
+
+
+    for i=1:length(idx)
+	X=[upts(i,:); centroids(idx(i),:)];
+	D=pdist(X,'Correlation');
+	if(D>Dth*1.00)
+	    idx_clean(i)=0;
+	end
     end
 
 end
